a8e98c84db04d1266edf5ac3de6e3df3b874096b
PatriciaChen1618 <pycc@LAPTOP-08RRSFOD.localdomain>
2022-03-30 00:46:48 +0800

    fea: heap

---
+++ kernel/src/mm/heap.rs
@@ -0,0 +1,20 @@
+use buddy_system_allocator::LockedHeap;
+use crate::config::KHEAP_SZ;
+
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+static mut HEAP_SPACE: [u8; KHEAP_SZ] = [0; KHEAP_SZ];
+
+pub fn init_heap() {
+    unsafe {
+        HEAP_ALLOCATOR
+            .lock()
+            .init(HEAP_SPACE.as_ptr() as usize, KHEAP_SZ);
+    }
+}
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
